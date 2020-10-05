438c9d54a7bf0a8a11723bf4e248ffa73f806811
Mark Miller <mark-miller04@outlook.com>
2020-10-05 16:01:03 -0400

    Add string to int method.

--- UtilityLibrary/StringUtils.cs
+++ UtilityLibrary/StringUtils.cs
@@ -2,7 +2,24 @@
 
 namespace UtilityLibrary
 {
-	public class StringUtils
+	public static class StringUtils
 	{
+		public static int StringToInt(string str)
+		{
+			int ret;
+			try {
+				ret = Convert.ToInt32(str);
+			}
+			catch(Exception e) {
+				throw new ArgumentException($"Invalid input caused by a {e.GetType().Name}.");
+			}
+
+			return ret;
+		}
+	}
+
+	public static class MathUtils
+	{
+
 	}
 }
