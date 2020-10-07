9ed01bc28e2346608fed23fb450c80a90f413114
Mark Miller <mark-miller04@outlook.com>
2020-10-07 14:09:48 -0400

    Add RequestSpecificInt to InputUtils.

--- UtilityLibrary/InputUtils.cs
+++ UtilityLibrary/InputUtils.cs
@@ -7,6 +7,11 @@ namespace UtilityLibrary
 {
 	public static class InputUtils
 	{
+		/// <summary>
+		/// Requests a valid integer from the user.
+		/// </summary>
+		/// <param name="msg">The question to prompt the user.</param>
+		/// <returns>A valid integer value.</returns>
 		public static int RequestInt(string msg)
 		{
 			Console.WriteLine(msg);
@@ -37,11 +42,47 @@ namespace UtilityLibrary
 		}
 
 		/// <summary>
+		/// Request an integer from the user. Only returns if the integer is in the acceptable set.
+		/// </summary>
+		/// <param name="acceptable">An array of acceptable integer inputs.</param>
+		/// <param name="msg">The question to prompt the user.</param>
+		/// <returns>An integer value that exists within the expected integer array.</returns>
+		public static int RequestSpecificInt(int[] acceptable, string msg)
+		{
+			Console.WriteLine(msg);
+			while (true)
+			{
+				string str = Console.ReadLine();
+				try {
+					int ret = StringUtils.StringToInt(str);
+					for (int i = 0; i < acceptable.Length; i++) {
+						if (ret == acceptable[i]) {
+							return ret;
+						}
+					}
+					Console.WriteLine("That integer is not a valid value, please try again.");
+				}
+				catch (FormatException) {
+					Console.WriteLine("Read failed due to characters other than digits. " + msg);
+					continue;
+				}
+				catch (OverflowException) {
+					Console.WriteLine("Value is too large, exceeding max integer size of 2,147,483,647. " + msg);
+					continue;
+				}
+				catch (Exception e) {
+					Console.WriteLine(e.GetType() + e.Message);
+					continue;
+				}
+			}
+		}
+
+		/// <summary>
 		/// Return a boolean value from the user's input based on a yes or no question.
 		/// </summary>
-		/// <param name="que">The question to ask the user.</param>
+		/// <param name="que">The question to prompt the user.</param>
 		/// <returns>True if answered yes, False if answered no.</returns>
-		public static bool YesNoQuestion(string que)
+		public static bool RequestYN(string que)
 		{
 			Console.WriteLine(que);
 			Console.WriteLine("Answer (y)es or (n)o: ");
